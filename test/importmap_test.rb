127570bb0e41e3ae9b8da88619c637a060f75bb2
David Heinemeier Hansson <david@loudthinking.com>
2021-08-02 13:35:44 -0400

    Better name

--- test/importmap_test.rb
+++ test/importmap_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ImportmapTest < ActiveSupport::TestCase
-  test "it has a version number" do
+  test "files in app/javascripts show up in importmap" do
     json = JSON.parse Rails.application.config.importmap.paths.to_json(ActionController::Base.helpers)
     assert_match %r|assets/application-.*\.js|, json["imports"]["application"]
   end
