05e3110fc978fd48ab32560ec4cb32bef0157eba
David Heinemeier Hansson <david@loudthinking.com>
2021-08-02 13:35:01 -0400

    Lazy-eval the setup instructions
    So we can pickup new files after compilation

--- lib/importmap/paths.rb
+++ lib/importmap/paths.rb
@@ -1,24 +1,17 @@
 class Importmap::Paths
-  attr_reader :paths
+  attr_reader :files, :directories
 
   def initialize
-    @paths = {}
+    @files = {}
+    @directories = {}
   end
 
   def asset(name, path: nil)
-    @paths[name] = path || "#{name}.js"
+    @files[name] = path || "#{name}.js"
   end
 
-  def assets_in(path)
-    if (absolute_path = absolute_root_of(path)).exist?
-      find_javascript_files_in_tree(absolute_path).each do |filename|
-        module_filename = filename.relative_path_from(absolute_path)
-        module_name     = module_name_from(module_filename)
-        module_path     = absolute_path.basename.join(module_filename).to_s
-
-        asset module_name, path: module_path
-      end
-    end
+  def assets_in(path, append_base_path: false)
+    @directories[path] = append_base_path
   end
 
   def to_json(resolver)
@@ -27,12 +20,30 @@ class Importmap::Paths
 
   private
     def map_to_asset_paths(resolver)
-      @paths.transform_values { |path| resolver.asset_path(path) }
+      expanded_files_and_directories.transform_values { |path| resolver.asset_path(path) }
     end
 
-    # Strip off the extension and any versioning data for an absolute module name.
+    def expanded_files_and_directories
+      @files.dup.tap { |expanded| expand_directories_into expanded }
+    end
+
+    def expand_directories_into(paths)
+      @directories.each do |(path, append_base_path)|
+        if (absolute_path = absolute_root_of(path)).exist?
+          find_javascript_files_in_tree(absolute_path).each do |filename|
+            module_filename = filename.relative_path_from(absolute_path)
+            module_name     = module_name_from(module_filename)
+            module_path     = append_base_path ? absolute_path.basename.join(module_filename).to_s : module_filename.to_s
+
+            paths[module_name] = module_path
+          end
+        end
+      end
+    end
+
+    # Strip off the extension, /index, or any versioning data for an absolute module name.
     def module_name_from(filename)
-      filename.to_s.remove(filename.extname).split("@").first
+      filename.to_s.remove(filename.extname).remove("/index").split("@").first
     end
 
     def find_javascript_files_in_tree(path)
