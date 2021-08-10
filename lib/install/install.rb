c2375af4b8c6303f55d0e7de1dacdc0b2c6adf06
David Heinemeier Hansson <david@loudthinking.com>
2021-08-10 12:39:09 +0200

    Configure importmap paths for Rails frameworks by default

--- lib/install/install.rb
+++ lib/install/install.rb
@@ -17,3 +17,20 @@ end
 
 say "Ensure JavaScript files are in the asset pipeline manifest"
 append_to_file Rails.root.join("app/assets/config/manifest.js"), %(//= link_tree ../javascripts)
+
+say "Configure importmap paths in config/initializers/assets.rb"
+append_to_file Rails.root.join("config/initializers/assets.rb") do <<-RUBY
+
+# Configure importmap paths in addition to having all files in app/assets/javascripts mapped.
+Rails.application.config.importmap.paths do |paths|
+  # Match libraries with their NPM package names for possibility of easy later porting.
+  # Ensure that libraries listed in the path have been linked in the asset pipeline manifest or precompiled.
+  paths.asset "@rails/actioncable", path: "actioncable.esm.js"
+  paths.asset "@rails/actiontext", path: "actiontext.js"
+  paths.asset "@rails/activestorage", path: "activestorage.esm.js"
+
+  # Make all files in directory available as my_channel => channels/my_channel-$digest.js
+  # paths.assets_in "lib/assets/javascripts/channels", append_base_path: true
+end
+RUBY
+end
