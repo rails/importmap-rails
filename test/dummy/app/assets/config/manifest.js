3c401331dfb24aa9e16dcf448d7e952500509adb
David Heinemeier Hansson <david@loudthinking.com>
2021-08-13 14:38:17 +0200

    Be specific about js files only

--- test/dummy/app/assets/config/manifest.js
+++ test/dummy/app/assets/config/manifest.js
@@ -1,4 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../javascripts
-//= link_tree ../../../lib/assets/javascripts
+//= link_tree ../javascripts .js
+//= link_tree ../../../lib/assets/javascripts .js
