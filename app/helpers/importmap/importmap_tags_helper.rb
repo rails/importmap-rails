57e7afa75237490b8f184cfa6cdeeacaabb0fd97
David Heinemeier Hansson <david@loudthinking.com>
2021-08-14 18:56:57 +0200

    Update for the new API

--- app/helpers/importmap/importmap_tags_helper.rb
+++ app/helpers/importmap/importmap_tags_helper.rb
@@ -8,8 +8,8 @@ module Importmap::ImportmapTagsHelper
     ], "\n"
   end
 
-  # Generate an inline importmap tag using the passed `importmap_paths` object to produce the JSON map.
-  # By default, `Rails.application.config.importmap.paths` is used for this object,
+  # Generate an inline importmap tag using the passed `importmap` object to produce the JSON map.
+  # By default, `Rails.application.config.importmap` is used for this object.
   def javascript_inline_importmap_tag(importmap = Rails.application.config.importmap)
     tag.script(importmap.to_json(self).html_safe, type: "importmap", "data-turbo-track": "reload")
   end
