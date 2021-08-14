9280007e89ab55e795d24a30128669251423d949
David Heinemeier Hansson <david@loudthinking.com>
2021-08-14 17:48:32 +0200

    Use direct importmap object

--- app/helpers/importmap/importmap_tags_helper.rb
+++ app/helpers/importmap/importmap_tags_helper.rb
@@ -10,8 +10,8 @@ module Importmap::ImportmapTagsHelper
 
   # Generate an inline importmap tag using the passed `importmap_paths` object to produce the JSON map.
   # By default, `Rails.application.config.importmap.paths` is used for this object,
-  def javascript_inline_importmap_tag(importmap_paths = Rails.application.config.importmap.paths)
-    tag.script(importmap_paths.to_json(self).html_safe, type: "importmap", "data-turbo-track": "reload")
+  def javascript_inline_importmap_tag(importmap = Rails.application.config.importmap)
+    tag.script(importmap.to_json(self).html_safe, type: "importmap", "data-turbo-track": "reload")
   end
 
   # Include the es-module-shim needed to make importmaps work in browsers without native support (like Firefox + Safari).
