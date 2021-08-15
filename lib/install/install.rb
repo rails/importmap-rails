fb7a3e4c41d1df57a34019d2985732ceac3bc35d
David Heinemeier Hansson <david@loudthinking.com>
2021-08-15 21:03:00 +0200

    Leave action text frameworks to action text installer
    And improve docs

--- lib/install/install.rb
+++ lib/install/install.rb
@@ -26,13 +26,9 @@ create_file Rails.root.join("config/initializers/importmap.rb") do <<-RUBY
 Rails.application.config.importmap.draw do
   pin "application"
 
-  # Match libraries with their NPM package names for possibility of later porting.
-  # Ensure that libraries listed in the path have been linked in the asset pipeline manifest or precompiled.
-  # Add the preload: true option to these pins to improve loading performance on browsers that support modulepreload links.
-  pin "@rails/actioncable", to: "actioncable.esm.js"
-  pin "@rails/activestorage", to: "activestorage.esm.js"
-  pin "@rails/actiontext", to: "actiontext.js"
-  pin "trix", to: "trix.js"
+  # Use libraries available via the asset pipeline (locally or via gems).
+  # pin "@rails/actioncable", to: "actioncable.esm.js"
+  # pin "@rails/activestorage", to: "activestorage.esm.js"
 
   # Use libraries directly from JavaScript CDNs (see https://www.skypack.dev, https://cdnjs.com, https://www.jsdelivr.com/esm)
   # pin "vue", to: "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js", preload: true
