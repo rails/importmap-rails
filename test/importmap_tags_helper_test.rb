3d073de354e4814cad0c7b1563ba24d53bf869bd
David Heinemeier Hansson <david@loudthinking.com>
2021-08-15 19:12:39 +0200

    More resilient match
    Don't fail on every change

--- test/importmap_tags_helper_test.rb
+++ test/importmap_tags_helper_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class Importmap::ImportmapTagsHelperTest < ActionView::TestCase
   test "javascript_inline_importmap_tag" do
-    assert_dom_equal \
-      %(<script type="importmap" data-turbo-track="reload">{"imports":{"md5":"https://cdn.skypack.dev/md5","not_there":"/nowhere.js","application":"/application.js","controllers/goodbye_controller":"/controllers/goodbye_controller.js"}}</script>),
+    assert_match \
+      %r{<script type="importmap" data-turbo-track="reload">{"imports":{"md5":"https://cdn.skypack.dev/md5".*}}</script>},
       javascript_inline_importmap_tag
   end
 
