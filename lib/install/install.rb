a87d588d716922f0550b3476a2687300b5e4417f
David Heinemeier Hansson <david@loudthinking.com>
2021-08-16 13:25:48 +0200

    preload: true is now default

--- lib/install/install.rb
+++ lib/install/install.rb
@@ -30,8 +30,8 @@ Rails.application.config.importmap.draw do
   # pin "@rails/activestorage", to: "activestorage.esm.js"
 
   # Use libraries directly from JavaScript CDNs (see https://www.skypack.dev, https://cdnjs.com, https://www.jsdelivr.com/esm)
-  # pin "vue", to: "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js", preload: true
-  # pin "d3", to: "https://cdn.skypack.dev/pin/d3@v7.0.0-03vFl9bie0TSesDkWTJV/mode=imports/optimized/d3.js", preload: true
+  # pin "vue", to: "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js"
+  # pin "d3", to: "https://cdn.skypack.dev/pin/d3@v7.0.0-03vFl9bie0TSesDkWTJV/mode=imports/optimized/d3.js"
 
   # Pin vendored modules by first adding the following to app/assets/config/manifest.js:
   # //= link_tree ../../../vendor/assets/javascripts .js
