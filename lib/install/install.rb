132d2e25cde83bbb301292dca2547aa2bc097c94
David Heinemeier Hansson <david@loudthinking.com>
2021-08-31 21:22:30 +0200

    Split importmap.rb template out of installer

--- lib/install/install.rb
+++ lib/install/install.rb
@@ -18,26 +18,5 @@ say "Ensure JavaScript files are in the asset pipeline manifest"
 append_to_file Rails.root.join("app/assets/config/manifest.js"), %(//= link_tree ../../javascript .js\n)
 
 say "Configure importmap paths in config/importmap.rb"
-create_file Rails.root.join("config/importmap.rb") do <<-RUBY
-# Use Action Cable channels (remember to import "@rails/actionable" in your application.js)
-# pin "@rails/actioncable", to: "actioncable.esm.js"
-# pin_all_from "app/javascript/channels", under: "channels"
+copy_file "#{__dir__}/config/importmap.rb", "config/importmap.rb"
 
-# Use direct uploads for Active Storage (remember to import "@rails/activestorage" in your application.js)
-# pin "@rails/activestorage", to: "activestorage.esm.js"
-
-# Pin vendored modules by first adding the following to app/assets/config/manifest.js:
-# //= link_tree ../../../vendor/assets/javascripts .js
-# pin_all_from "vendor/assets/javascripts"
-
-# JavaScript CDN provider. Also available: :jsdelivr, :esmsh, :unpkg, :skypack
-provider :jspm
-
-# Use NPM libraries from CDN
-# pin "local-time", version: "2.1.0", file: "app/assets/javascripts/local-time.js"
-# pin "vue", version: "2.6.14", file: "dist/vue.esm.browser.js", from: :jsdelivr
-# pin "d3", version: "7.0.1", file: "?bundle", from: :esmsh
-
-pin "application"
-RUBY
-end
