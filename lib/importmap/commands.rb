d5c997abc8cd5dfd2ecf2417da542fa063a05bf9
David Heinemeier Hansson <david@loudthinking.com>
2021-08-31 13:14:18 +0200

    Add a pin CLI command

---
+++ lib/importmap/commands.rb
@@ -0,0 +1,24 @@
+require "thor"
+require "importmap/pinner"
+
+class Importmap::Commands < Thor
+  desc "pin [*PACKAGES]", "Pin new packages"
+  option :env, type: :string, aliases: :e, default: "production"
+  option :provider, type: :string, aliases: :p, default: "jspm"
+  def pin(*packages)
+    if imports = Importmap::Pinner.new.pin(*packages, env: options[:env], provider: options[:provider])
+      imports.each do |package, url|
+        puts "Pinned '#{package}' to #{url}"
+      end
+    else
+      puts "Couldn't find any packages in #{packages.inspect} on #{options[:provider]}"
+    end
+  end
+
+  desc "json", "Show the full importmap in json"
+  def json
+    puts Rails.application.config.importmap.to_json(resolver: ActionController::Base.helpers)
+  end
+end
+
+Importmap::Commands.start(ARGV)
