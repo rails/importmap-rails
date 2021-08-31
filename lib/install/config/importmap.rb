5b77c5fd7bbe26b5b6eb9685d08e60ae7d4b648c
David Heinemeier Hansson <david@loudthinking.com>
2021-08-31 22:51:19 +0200

    Update template importmap.rb for the new focus

--- lib/install/config/importmap.rb
+++ lib/install/config/importmap.rb
@@ -5,16 +5,6 @@
 # Use direct uploads for Active Storage (remember to import "@rails/activestorage" in your application.js)
 # pin "@rails/activestorage", to: "activestorage.esm.js"
 
-# Pin vendored modules by first adding the following to app/assets/config/manifest.js:
-# //= link_tree ../../../vendor/assets/javascripts .js
-# pin_all_from "vendor/assets/javascripts"
-
-# JavaScript CDN provider. Also available: :jsdelivr, :esmsh, :unpkg, :skypack
-provider :jspm
-
-# Use NPM libraries from CDN
-# pin "local-time", version: "2.1.0", file: "app/assets/javascripts/local-time.js"
-# pin "vue", version: "2.6.14", file: "dist/vue.esm.browser.js", from: :jsdelivr
-# pin "d3", version: "7.0.1", file: "?bundle", from: :esmsh
+# Use node modules from a JavaScript CDN by running ./bin/importmap
 
 pin "application"
