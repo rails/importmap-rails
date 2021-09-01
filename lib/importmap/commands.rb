dec0901b762bed3541e4fb5168e14e4b3ff02845
David Heinemeier Hansson <david@loudthinking.com>
2021-09-01 11:10:53 +0200

    Use new Packager and offer #unpin as well

--- lib/importmap/commands.rb
+++ lib/importmap/commands.rb
@@ -1,14 +1,39 @@
 require "thor"
-require "importmap/pinner"
+require "importmap/packager"
 
 class Importmap::Commands < Thor
+  include Thor::Actions
+  
   desc "pin [*PACKAGES]", "Pin new packages"
   option :env, type: :string, aliases: :e, default: "production"
   option :from, type: :string, aliases: :f, default: "jspm"
   def pin(*packages)
-    if imports = Importmap::Pinner.new.pin(*packages, env: options[:env], from: options[:from])
+    if imports = packager.import(*packages, env: options[:env], from: options[:from])
       imports.each do |package, url|
-        puts "Pinned '#{package}' to #{url}"
+        pin = packager.pin_for(package, url)
+
+        puts pin
+
+        if packager.packaged?(package)
+          gsub_file("config/importmap.rb", /^pin "#{package}".*$/, pin)
+        else
+          append_to_file("config/importmap.rb", "#{pin}\n")
+        end
+      end
+    else
+      puts "Couldn't find any packages in #{packages.inspect} on #{options[:provider]}"
+    end
+  end
+
+  desc "unpin [*PACKAGES]", "Unpin existing packages"
+  option :env, type: :string, aliases: :e, default: "production"
+  option :from, type: :string, aliases: :f, default: "jspm"
+  def unpin(*packages)
+    if imports = packager.import(*packages, env: options[:env], from: options[:from])
+      imports.each do |package, url|
+        if packager.packaged?(package)
+          gsub_file("config/importmap.rb", /^pin "#{package}".*$/, "")
+        end
       end
     else
       puts "Couldn't find any packages in #{packages.inspect} on #{options[:provider]}"
@@ -19,6 +44,11 @@ class Importmap::Commands < Thor
   def json
     puts Rails.application.config.importmap.to_json(resolver: ActionController::Base.helpers)
   end
+
+  private
+    def packager
+      @packager ||= Importmap::Packager.new
+    end
 end
 
 Importmap::Commands.start(ARGV)
