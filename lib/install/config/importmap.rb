65d0f2255ba62b4c4ccb03993945772780d52a19
David Heinemeier Hansson <david@loudthinking.com>
2021-09-03 20:47:45 +0200

    This is now setup by channel generator

--- lib/install/config/importmap.rb
+++ lib/install/config/importmap.rb
@@ -1,7 +1,3 @@
-# Use Action Cable channels (remember to import "@rails/actionable" in your application.js)
-# pin "@rails/actioncable", to: "actioncable.esm.js"
-# pin_all_from "app/javascript/channels", under: "channels"
-
 # Use direct uploads for Active Storage (remember to import "@rails/activestorage" in your application.js)
 # pin "@rails/activestorage", to: "activestorage.esm.js"
 
