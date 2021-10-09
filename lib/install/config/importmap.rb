bf362a6ed2e1600bc1b3175a8f98fda348c0fd54
David Heinemeier Hansson <david@loudthinking.com>
2021-10-09 15:49:26 +0200

    Odd to focus on AS first

--- lib/install/config/importmap.rb
+++ lib/install/config/importmap.rb
@@ -1,6 +1,3 @@
-# Use direct uploads for Active Storage (remember to import "@rails/activestorage" in your application.js)
-# pin "@rails/activestorage", to: "activestorage.esm.js"
-
-# Use npm packages from a JavaScript CDN by running ./bin/importmap
+# Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
