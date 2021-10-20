256f975f8d78bc6e49ad5db7b8cb6e0ecb35908b
Dan Laffan <dan.laffan@mac.com>
2021-10-20 20:10:45 +0100

    Enable install.rb to handle Rails engines

--- lib/install/install.rb
+++ lib/install/install.rb
@@ -1,32 +1,46 @@
-APPLICATION_LAYOUT_PATH = Rails.root.join("app/views/layouts/application.html.erb")
+# frozen_string_literal: true
 
-if APPLICATION_LAYOUT_PATH.exist?
-  say "Add Importmap include tags in application layout"
-  insert_into_file APPLICATION_LAYOUT_PATH.to_s, "\n    <%= javascript_importmap_tags %>", before: /\s*<\/head>/
-else
-  say "Default application.html.erb is missing!", :red
-  say "        Add <%= javascript_importmap_tags %> within the <head> tag in your custom layout."
+# This file is called by +lib/tasks/importmap_tasks.rake+.
+
+rails_root = Pathname.new(Rails.root.to_s.split(%r{\/(spec|test)\/dummy}).first)
+say "Adding configuration for the importmap-rails gem into: #{rails_root}"
+
+Dir.glob("#{rails_root}/**/application.html.*").each do |layout_path|
+  say "Add Importmap include tags in application layout file at #{layout_path}"
+  spaces = IO.foreach(layout_path).grep(/(\s*)body/).first.match(/(\s*)/)[1]
+  case layout_path.split('.html.').last
+  when 'erb'
+    insert_into_file layout_path.to_s, "\n#{spaces}  <%= javascript_importmap_tags %>", before: /\s*<\/head>/
+  when 'slim'
+    insert_into_file layout_path.to_s, "\n#{spaces}  = javascript_importmap_tags", before: /\s*body/
+  when 'haml'
+    insert_into_file layout_path.to_s, "\n#{spaces}  =javascript_importmap_tags", before: /\s*%body/
+  else
+    say "Couldn't find an application.html.erb|haml|slim file!", :red
+    say "        Add <%= javascript_importmap_tags %> within the <head> tag into your custom layout."
+  end
 end
 
 say "Create application.js module as entrypoint"
-create_file Rails.root.join("app/javascript/application.js") do <<-JS
+create_file rails_root.join("app/javascript/application.js") do <<-JS
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 JS
 end
 
 say "Use vendor/javascript for downloaded pins"
-empty_directory "vendor/javascript"
-keep_file "vendor/javascript"
+empty_directory rails_root.join("vendor/javascript")
+keep_file rails_root.join("vendor/javascript")
 
-if (sprockets_manifest_path = Rails.root.join("app/assets/config/manifest.js")).exist?
+sprockets_manifest_path = rails_root.join("app/assets/config/manifest.js")
+if sprockets_manifest_path.exist?
   say "Ensure JavaScript files are in the Sprocket manifest"
   append_to_file sprockets_manifest_path,
     %(//= link_tree ../../javascript .js\n//= link_tree ../../../vendor/javascript .js\n)
 end
 
 say "Configure importmap paths in config/importmap.rb"
-copy_file "#{__dir__}/config/importmap.rb", "config/importmap.rb"
+copy_file "#{__dir__}/config/importmap.rb", rails_root.join("config/importmap.rb")
 
 say "Copying binstub"
-copy_file "#{__dir__}/bin/importmap", "bin/importmap"
-chmod "bin", 0755 & ~File.umask, verbose: false
+copy_file "#{__dir__}/bin/importmap", rails_root.join("bin/importmap")
+chmod rails_root.join("bin"), 0755 & ~File.umask, verbose: false
