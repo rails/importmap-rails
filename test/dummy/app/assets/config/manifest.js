557766d2a4737b144860e292900c8088b5394749
Jason Taylor <jason@redant.com.au>
2023-07-19 10:07:25 +1000

    Add failing test to cover empty to value

--- test/dummy/app/assets/config/manifest.js
+++ test/dummy/app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
+//= link_tree ../../components .js
 //= link_tree ../../../lib/assets/javascripts .js
