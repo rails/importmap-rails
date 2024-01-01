31ced447db943c63ed889112b60fb0ca5562340b
David Heinemeier Hansson <david@basecamp.com>
2024-01-01 14:10:16 +0100

    Fix test fixtures

--- test/dummy/config/importmap.rb
+++ test/dummy/config/importmap.rb
@@ -1,4 +1,4 @@
 pin_all_from "app/assets/javascripts"
 
 pin "md5", to: "https://cdn.skypack.dev/md5", preload: true
-pin "not_there", to: "nowhere.js"
+pin "not_there", to: "nowhere.js", preload: false
