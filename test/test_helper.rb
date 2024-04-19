0a6330133b75a72d7c5d07fe87c2fce50d76b997
Rafael Mendonça França <rafael@rubyonrails.org>
2024-04-19 00:36:36 +0000

    Fix test with Rails main
    `fixture_path` was removed in Rails 7.2.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,9 +8,18 @@ require "rails/test_help"
 
 
 # Load fixtures from the engine
+# Remove this when support to Rails 7.0 is dropped.
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
   ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
+  ActiveSupport::TestCase.fixtures :all
+end
+
+# Load fixtures from the engine
+if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
+  ActiveSupport::TestCase.fixture_paths = [File.expand_path("fixtures", __dir__)]
+  ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
+  ActiveSupport::TestCase.file_fixture_path = File.expand_path("fixtures", __dir__) + "/files"
   ActiveSupport::TestCase.fixtures :all
 end
