b360120df57835b7bcc90efa5b22b75ea4879896
Corné Verbruggen <info@corneverbruggen.com>
2024-10-05 02:22:21 +0200

    Use entry_point as cache_key when generating preload tags (#266)

--- app/helpers/importmap/importmap_tags_helper.rb
+++ app/helpers/importmap/importmap_tags_helper.rb
@@ -25,7 +25,7 @@ module Importmap::ImportmapTagsHelper
   # (defaults to Rails.application.importmap), such that they'll be fetched
   # in advance by browsers supporting this link type (https://caniuse.com/?search=modulepreload).
   def javascript_importmap_module_preload_tags(importmap = Rails.application.importmap, entry_point: "application")
-    javascript_module_preload_tag(*importmap.preloaded_module_paths(resolver: self, entry_point:))
+    javascript_module_preload_tag(*importmap.preloaded_module_paths(resolver: self, entry_point:, cache_key: entry_point))
   end
 
   # Link tag(s) for preloading the JavaScript module residing in `*paths`. Will return one link tag per path element.
