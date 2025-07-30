19094ecd13017f411494a8a743c0da667873d75a
Rafael Mendonça França <rafael@rubyonrails.org>
2025-07-30 19:03:32 +0000

    Improve SRI support
    Remove the static integrity download from the `pin` and related commands.
    
    This can't work because we modify the asset when vendoring in the application
    to add a comment about the source of the asset.
    
    Instead, we are by default delegating integrity calculation to the Rails
    asset pipeline.
    
    Fixes #308.

--- lib/importmap/commands.rb
+++ lib/importmap/commands.rb
@@ -13,19 +13,15 @@ class Importmap::Commands < Thor
   option :env, type: :string, aliases: :e, default: "production"
   option :from, type: :string, aliases: :f, default: "jspm"
   option :preload, type: :string, repeatable: true, desc: "Can be used multiple times"
-  option :integrity, type: :boolean, aliases: :i, default: true, desc: "Include integrity hash from JSPM"
   def pin(*packages)
-    with_import_response(packages, env: options[:env], from: options[:from], integrity: options[:integrity]) do |imports, integrity_hashes|
-      process_imports(imports, integrity_hashes) do |package, url, integrity_hash|
-        puts %(Pinning "#{package}" to #{packager.vendor_path}/#{package}.js via download from #{url})
+    for_each_import(packages, env: options[:env], from: options[:from]) do |package, url|
+      puts %(Pinning "#{package}" to #{packager.vendor_path}/#{package}.js via download from #{url})
 
-        packager.download(package, url)
+      packager.download(package, url)
 
-        pin = packager.vendored_pin_for(package, url, options[:preload], integrity: integrity_hash)
+      pin = packager.vendored_pin_for(package, url, options[:preload])
 
-        log_integrity_usage(integrity_hash)
-        update_importmap_with_pin(package, pin)
-      end
+      update_importmap_with_pin(package, pin)
     end
   end
 
@@ -33,12 +29,10 @@ class Importmap::Commands < Thor
   option :env, type: :string, aliases: :e, default: "production"
   option :from, type: :string, aliases: :f, default: "jspm"
   def unpin(*packages)
-    with_import_response(packages, env: options[:env], from: options[:from]) do |imports, _integrity_hashes|
-      imports.each do |package, url|
-        if packager.packaged?(package)
-          puts %(Unpinning and removing "#{package}")
-          packager.remove(package)
-        end
+    for_each_import(packages, env: options[:env], from: options[:from]) do |package, url|
+      if packager.packaged?(package)
+        puts %(Unpinning and removing "#{package}")
+        packager.remove(package)
       end
     end
   end
@@ -46,18 +40,13 @@ class Importmap::Commands < Thor
   desc "pristine", "Redownload all pinned packages"
   option :env, type: :string, aliases: :e, default: "production"
   option :from, type: :string, aliases: :f, default: "jspm"
-  option :integrity, type: :boolean, aliases: :i, default: true, desc: "Include integrity hash from JSPM"
   def pristine
     packages = prepare_packages_with_versions
 
-    with_import_response(packages, env: options[:env], from: options[:from], integrity: options[:integrity]) do |imports, integrity_hashes|
-      process_imports(imports, integrity_hashes) do |package, url, integrity_hash|
-        puts %(Downloading "#{package}" to #{packager.vendor_path}/#{package}.js from #{url})
+    for_each_import(packages, env: options[:env], from: options[:from]) do |package, url|
+      puts %(Downloading "#{package}" to #{packager.vendor_path}/#{package}.js from #{url})
 
-        packager.download(package, url)
-
-        log_integrity_usage(integrity_hash)
-      end
+      packager.download(package, url)
     end
   end
 
@@ -118,33 +107,6 @@ class Importmap::Commands < Thor
     puts npm.packages_with_versions.map { |x| x.join(' ') }
   end
 
-  desc "integrity [*PACKAGES]", "Download and add integrity hashes for packages"
-  option :env, type: :string, aliases: :e, default: "production"
-  option :from, type: :string, aliases: :f, default: "jspm"
-  option :update, type: :boolean, aliases: :u, default: false, desc: "Update importmap.rb with integrity hashes"
-  def integrity(*packages)
-    packages = prepare_packages_with_versions(packages)
-
-    with_import_response(packages, env: options[:env], from: options[:from], integrity: true) do |imports, integrity_hashes|
-      process_imports(imports, integrity_hashes) do |package, url, integrity_hash|
-        puts %(Getting integrity for "#{package}" from #{url})
-
-        if integrity_hash
-          puts %(  #{package}: #{integrity_hash})
-
-          if options[:update]
-            pin_with_integrity = packager.pin_for(package, url, integrity: integrity_hash)
-
-            update_importmap_with_pin(package, pin_with_integrity)
-            puts %(  Updated importmap.rb with integrity for "#{package}")
-          end
-        else
-          puts %(  No integrity hash available for "#{package}")
-        end
-      end
-    end
-  end
-
   private
     def packager
       @packager ||= Importmap::Packager.new
@@ -160,10 +122,6 @@ class Importmap::Commands < Thor
       else
         append_to_file("config/importmap.rb", "#{pin}\n", verbose: false)
       end
-    end
-
-    def log_integrity_usage(integrity_hash)
-      puts %(  Using integrity: #{integrity_hash}) if integrity_hash
     end
 
     def handle_package_not_found(packages, from)
@@ -205,18 +163,11 @@ class Importmap::Commands < Thor
       end
     end
 
-    def process_imports(imports, integrity_hashes, &block)
-      imports.each do |package, url|
-        integrity_hash = integrity_hashes[url]
-        block.call(package, url, integrity_hash)
-      end
-    end
-
-    def with_import_response(packages, **options)
+    def for_each_import(packages, **options, &block)
       response = packager.import(*packages, **options)
 
       if response
-        yield response[:imports], response[:integrity]
+        response[:imports].each(&block)
       else
         handle_package_not_found(packages, options[:from])
       end
--- test/dummy/config/importmap.rb
+++ test/dummy/config/importmap.rb
@@ -1,5 +1,5 @@
 pin_all_from "app/assets/javascripts"
 
-pin "md5", to: "https://cdn.skypack.dev/md5", preload: true
-pin "not_there", to: "nowhere.js", preload: false
+pin "md5", to: "https://cdn.skypack.dev/md5", preload: true, integrity: false
+pin "not_there", to: "nowhere.js", preload: false, integrity: false
 pin "rich_text", preload: true, integrity: "sha384-OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb"
